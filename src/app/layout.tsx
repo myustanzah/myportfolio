3417a56651ef733c0bf25a96ee52c9dc9e299fe6
yustanzah <yustanzah@nds.co.id>
2025-07-11 10:37:21 +0700

    fix5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,9 +12,9 @@ const roboto = Roboto({
 });
 
 export const metadata: Metadata = {
-  title: "Next.js + Tailwind Portfolio Page",
+  title: "My Portfolio",
   description:
-    "Introducing Tailwind Portfolio Page, a versatile and stylish portfolio template built on the foundation of Tailwind CSS and Material Tailwind.",
+    "All about me, everythink in here",
 };
 
 export default function RootLayout({
